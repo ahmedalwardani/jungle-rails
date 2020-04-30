25bac3447eb7a9a37438f8402b03df0ed4a18b3c
ahmedalwardani <aalwa049@uottawa.ca>
2020-04-30 00:03:36 +0000

    add about us awesome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
-
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :products, only: %i[index show]
+  resources :categories, only: %i[show]
 
-  resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+  resource :cart, only: %i[show] do
+    post :add_item
+    post :remove_item
   end
 
-  resources :orders, only: [:create, :show]
+  resources :orders, only: %i[create show]
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products, except: %i[edit update show]
   end
 
+  resources :about, only: %i[index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
