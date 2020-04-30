e361eef545158a58c95f6554d2997386764a73e3
ahmedalwardani <aalwa049@uottawa.ca>
2020-04-30 18:07:06 +0000

    add categories controller WIP

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,13 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create; end
+end
