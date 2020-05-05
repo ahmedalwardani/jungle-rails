aad2c1bacfe7bb52fd885dfe3e1248be169bee3e
ahmedalwardani <aalwa049@uottawa.ca>
2020-05-05 02:26:03 +0000

    account for case and whitespaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, presence: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
-  validates_uniqueness_of :email, case_sensitive: false
+  validates :password, presence: true, length: { minimum: 8 }
   has_secure_password
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    return user if user && user.authenticate(password)
+  end
 end
