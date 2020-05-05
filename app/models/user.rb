6d4ac4998a2e83eb751d51aca0249f5375e8d405
ahmedalwardani <aalwa049@uottawa.ca>
2020-05-05 01:45:21 +0000

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+  validates_uniqueness_of :email, case_sensitive: false
   has_secure_password
 end
